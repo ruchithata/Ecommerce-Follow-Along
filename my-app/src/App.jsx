08f534328a1ebc890f1bc6f353e4851e02699a77
T A Ruchitha <ruchitha.ta83@gmail.com>
2025-01-29 12:37:49 +0530

    product

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import { Login } from './Components/Login'
 import { Signup } from './Components/Signup'
+import { Home } from './page/Home'
 
 function App() {
 
   return (
     <>
     <Routes>
+      <Route path="/Home" element={<Home/>} />
       <Route path="/Login" element={<Login/>} />
       <Route path="/Signup" element={<Signup/>} />
     </Routes>
