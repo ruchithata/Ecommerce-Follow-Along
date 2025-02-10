3f71945ff9e4c7b856242e22570f98ff7ab48c01
T A Ruchitha <ruchitha.ta83@gmail.com>
2025-02-10 13:40:05 +0530

    imageA

--- my-app/src/Components/Product.jsx
+++ my-app/src/Components/Product.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 
 
 
 export const Productcard = ({image,name,price,description}) => {
+
+  const {currentIndex, setcurrentIndex} = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setcurrentIndex((prev) => (prev + 1) % image.length)
+    },2000)
+    return () => {
+      clearInterval(interval)
+    }
+
+  }, [image] )
+
+  const currentImage = image[currentIndex];
+  
+
   return (
     <div className="bg-neutral-200 p-4 rounded-lg shadow-md flex justify-between flex-col">
       <div className='w-full'>
