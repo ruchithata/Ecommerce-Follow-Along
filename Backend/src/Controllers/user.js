c0bbe5d16a4d15045cf6302bd3978b9aa670adad
T A Ruchitha <ruchitha.ta83@gmail.com>
2025-03-27 15:36:11 +0530

    Update user.js

--- Backend/src/Controllers/user.js
+++ Backend/src/Controllers/user.js
@@ -1,140 +1,158 @@
 const {Router}= require("express");
 const userModel = require("../Model/userModel");
 const { upload } = require("../../multer");
-const {bcrypt} = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
 const userrouter = Router();
-const jwt = require("jsonwebtoken");
+const AsyncError = require('../Middleware/catchAsyncError');
 const auth = require("../Middleware/auth");
-require("dotenv").config({ path: "./src/config/.env" });
+require('dotenv').config({  path:'./src/config/.env'});
 
-const secret = process.env.private_key;
+const secret = process.env.secretkey;
 
-userrouter.post("/create-user",upload.single("file"), async(req,res)=>{
+userrouter.post("/create-user",upload.single('file'),async(req,res)=>{
     const {name, email, password} = req.body;
     const userEmail = await userModel.findOne({email});
     if (userEmail) {
         return next(new ErrorHandler("User already exists", 400));
       }
 
-      const filename = req.file.filename ; 
+      const filename = req.file.filename ;
       const fileUrl = path.join(filename);
+   
+    bcrypt.hash(password, 10,async function(err, hash) {
 
-      bcrypt.hash(password, 10, async function(err, hash) {
-      
-      await userModel.create({
-        name:name,
-        email:email,
-        password: hash,
-        // avatar:fileUrl
-      })
-    })
+        await userModel.create({
+            name:name,
+            email:email,
+            password:hash,
+            // avatar:fileUrl
 
-console.log(user);
-});
+        })
 
-userrouter.post("/login", async(req,res)=>{
-    const {email, password} = req.body;
-    const check = await userModel.findOne({email:email});
-    console.log(check);
-
-    if (!check) {
-        return res.status(400).json({message: "User not found"});
-    }
-
-    bcrypt.compare(password, check.password, function(err, result){
-      if (err) {
-        return res.status(400).json({message: "Invalid bcrypt compare"});
-      }
-      if (result) {
-
-        jwt.sign({email:email},xyz,(err,token)=>{
-          if (err) {
-            return res.status(400).json({message: "Invalid jwt"});
-          }
-          res.setHeader("Autherization", `Bearer ${token}`);
-          console.log(token);
-          res.status(200).json({token:token});
         })
 
 
-      }
-      else {
-        return res.status(400).json({message: "Invalid password"});
-      } 
-    })
-
 });
 
 
+userrouter.post("/login",async(req,res)=>{
+    const {email, password} = req.body;
+    const check= await userModel.findOne({email:email});
+        console.log(check);
+    if(!check){
+        return res.status(400).json({message:"User not found"});
+    }
+
+    bcrypt.compare(password,check.password , function(err, result) {
+       
+        if(err){
+            return res.status(400).json({message:"Invalid bcrpyt compare"});
+        }
+        if(result){
+            
+            jwt.sign({email:email},secret,(err,token)=>{
+                if(err){
+                    return res.status(400).json({message:"Invalid jwt"});
+                }
+                res.cookie('autherization',token,{
+                    expires:new Date(Date.now() + 900000),
+                    httpOnly:true,
+                    domain:'.localhost.com'
+                })
+                console.log(token);
+                res.status(200).json({token:token});    
+            })
 
 
-// userrouter.post('/add-address', auth, async (req, res) => {
-//     const {
-//       country,
-//       city,
-//       address1,
-//       address2,
-//       zipCode,
-//       addressType,email
-//     } = req.body;
-//     try {
-//         if (!address || !city || !state || !zip || !country) {
-//             return res.status(400).json({ message: "Fill all fields" });
-//         }
-//         const addressDetails = new userModel({ address, city, state, zip, country });
-//         await addressDetails.save();
-//         res.status(200).json({ message: "Address added successfully" });
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ message: "Internal Server Error" });
-//     }
-// });
+        }   
+        else{
+            return res.status(400).json({message:"Invalid password"});
+        }
+    });
+
+})
+
+
+userrouter.get('/profile',async (req,res) => {
+    const {email} = req.query;
+    if(!email){
+        return res.status(400).json({message:"email cannot be empty!"})
+    }
+
+    try{
+    const user = await userModel.findOne({email:email});
+
+    if(!user){
+        return res.status(404).json({message:"The user was not found"});
+    }
+
+    const Users = {
+        name: user.name,
+        email: user.email,
+        phone: user.phoneNumber,
+        image: user.avatarurl,
+        address: user.address
+    }
+
+    res.status(200).json({message:"successfully recieved"});
+}
+
+catch(err){
+    res.status(500).json("error":err);
+}
+})
+
 
 
 
 userrouter.post('/add-address',auth, async(req,res)=>{
 
-  try{
-    const email = req.body;
+    try{
+
+        const email=req.user
     const {country,
-      city,
-      address1,
-      address2,
-      zipCode,
-      addressType}=req.body
+        city,
+        address1,
+        address2,   
+        zipCode,
+        addressType}=req.body
 
-      const user=await userModel.find({email:email})
+        const user=await userModel.find({email:email})
 
-    const newaddress={
-      country,
-      city,
-      address1,
-      address2,
-      zipCode,
-      addressType,
-  }
+      const newaddress={
+        country,
+        city,
+        address1,
+        address2,
+        zipCode,
+        addressType,
+    }
 
-  user.addresses.push(newaddress)
-  await user.save()
+    user.addresses.push(newaddress)
+    await user.save()
 }
 catch(err){
-  console.log("error in address",err)
+    console.log("error in address",err)
 }
-});
+})
 
-
-userrouter.get('/get-address', auth,async(req,res)=>{ 
-  const email = req.user
-  try{
-    const user = await userModel.findOne({email:email})
+userrouter.get('/get-address',auth,async(req,res)=>{
+   const email=req.user
+   try{
+    const user=await userModel.findOne({email:email})
     if(!user){
-      return res.status(400).json({message: "user not found"});
-    }
-    res.status(200).json({message: "successfully recieved.",user:user.addresses});
-  }
-  catch(err){
-    console.log("error in getting address.",err);
-  }
+        return res.status(400).json({message:"User not found"})
+    }   
+    res.status(200).json({message:"successfully recieved",user:user.addresses
+    })
+   }
+   catch(err){
+         console.log("error in get address",err)    
+   }
+
+
+
 })
 
 
